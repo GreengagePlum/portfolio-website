89fa8bb289c296e827e7bc68888c7d28bae64610
Efe ERKEN <efeerkenn@hotmail.com>
2023-03-22 17:36:07 +0100

    :wrench: UPDATE: Seperate article HTML element
    Seperated article element from HTML to avoid repetition.

---
+++ assets/templates/article.php
@@ -0,0 +1,18 @@
+<article>
+    <header>
+        <img src="" alt="" />
+        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+    </header>
+    <main>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec sapien ac neque bibendum
+            dignissim. In dui orci, sodales non luctus non, dignissim luctus odio. Mauris a nisl tortor.
+            Proin porta rutrum metus, eget placerat urna ornare quis. Ut mattis leo at felis venenatis
+            congue in at purus. Integer sit amet ex in magna malesuada consequat sit amet laoreet orci.
+            Nullam at velit et leo viverra semper id quis quam. Aenean iaculis arcu sit amet consequat
+            suscipit.</p>
+    </main>
+    <footer>
+        <button type="button"><i class="mdi mdi-hand-clap"></i></button>
+        <?php require("assets/templates/social-media.php"); ?>
+    </footer>
+</article>
