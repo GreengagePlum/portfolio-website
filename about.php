44342302fb9c7859eb812ce711a5f8797dc717fe
Efe ERKEN <efeerkenn@hotmail.com>
2023-03-24 11:57:18 +0100

    :wrench: UPDATE: Add placeholder text
    Added placeholder text and a temporary title to the about page.

---
+++ about.php
@@ -0,0 +1,31 @@
+<?php require_once("assets/php/lang.php"); ?>
+<!DOCTYPE html>
+<html lang="<?= $LANG_CURR ?>" dir="ltr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Efe Erken</title>
+    <link rel="icon" type="image/x-icon" href="/assets/img/logo.svg" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.1.96/css/materialdesignicons.min.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <script src="assets/js/script.js" defer></script>
+</head>
+
+<body>
+    <?php require_once("assets/templates/header.php"); ?>
+    <main>
+        <h1>Lorem Ipsum</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque convallis justo neque. Praesent molestie semper leo ut cursus. Suspendisse tincidunt, lorem ac venenatis luctus, lorem diam feugiat ipsum, id pulvinar dolor est in eros. Nunc vitae sem risus. Nullam eleifend orci vitae elit convallis, et interdum velit faucibus. Mauris lacinia orci non velit congue tincidunt. Aliquam faucibus tincidunt nisl et dictum.
+            Vivamus varius, arcu vitae interdum interdum, ligula augue posuere felis, vel malesuada elit ante et mauris. Aenean vitae lacus nec libero mattis viverra. Pellentesque scelerisque accumsan sodales. Maecenas feugiat aliquam ultrices. Nullam id auctor enim. Sed sagittis eros condimentum neque maximus mollis. Phasellus bibendum enim a congue pulvinar. Morbi cursus vehicula quam non vestibulum. Proin a rutrum sem, at posuere augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse nec porttitor eros. Mauris dictum laoreet commodo. Vivamus convallis posuere diam gravida tincidunt. Pellentesque fringilla, turpis ut cursus lobortis, ante ante aliquet leo, eget placerat erat massa at metus. Cras quis arcu sit amet massa mollis aliquet in quis turpis. Sed commodo lorem nulla.
+            Praesent malesuada eget massa nec convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum suscipit sem ut arcu sollicitudin, sit amet tincidunt nisi scelerisque. Aliquam erat volutpat. Aenean et ullamcorper magna. Proin ac ante sapien. Sed eu magna nec justo consequat aliquam. Nullam a lorem vitae purus eleifend ornare. Quisque vehicula consequat eros, ut volutpat magna dignissim et. Nullam ac nibh et orci tempus dictum non pretium lacus. Nunc dictum felis ac magna vehicula blandit. Fusce venenatis enim vitae semper feugiat.
+            Aenean lobortis eros at diam euismod bibendum. Donec consectetur purus eget velit egestas viverra. Integer consequat, arcu vitae ornare commodo, purus sapien sagittis risus, in tempus orci est vel turpis. Maecenas et consequat dolor. Pellentesque tristique vitae leo non ultricies. Praesent imperdiet malesuada erat in pharetra. Suspendisse facilisis sem a faucibus tempus. Aenean lobortis laoreet ligula vel lobortis. Proin dictum imperdiet luctus. Nam nec vehicula est.
+            Quisque commodo dui mi. Duis augue diam, auctor non justo quis, lacinia ultricies felis. Etiam nibh quam, malesuada non purus nec, condimentum ullamcorper urna. Nulla facilisi. In maximus nisi in dignissim pharetra. Maecenas vehicula eros in odio accumsan volutpat. Vestibulum leo massa, tempus nec mi vitae, scelerisque efficitur massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer bibendum aliquam turpis et placerat. Maecenas sollicitudin erat a sapien vestibulum egestas eget at lectus. Nam consectetur suscipit massa quis sagittis. Donec suscipit lectus ac dolor iaculis auctor. Curabitur eu augue tellus.
+            Generated 5 paragraphs, 428 words, 2978 bytes of Lorem Ipsum
+        </p>
+    </main>
+    <?php require_once("assets/templates/footer.php"); ?>
+</body>
+
+</html>
