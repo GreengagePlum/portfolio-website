4287ecac5d29cc7dc08c55494bc1869f6a8c56e1
Efe ERKEN <efeerkenn@hotmail.com>
2023-03-24 12:02:22 +0100

    :wrench: UPDATE: Add more translations
    Added more entries to the translation table for more elements.

--- assets/locales/en.php
+++ assets/locales/en.php
@@ -14,6 +14,25 @@ $lang = [
         "alt-text" => "Two e letters stacked one on another"
     ],
     "button" => [
-        "see-more" => "See more posts"
+        "see-more" => "See more posts",
+        "submit" => "Submit"
     ],
+    "admin" => [
+        "h1" => "Insert an Article into the Database"
+    ],
+    "form" => [
+        "label" => [
+            "title" => "Title",
+            "description" => "Description",
+            "image" => "Picture",
+            "image-alt" => "Alternative text for the picture"
+        ],
+        "input" => [
+            "title" => "A personal portfolio website",
+            "image-alt" => "A screenshot of my website showing the login screen"
+        ],
+        "textarea" => [
+            "description" => "I made a website using PHP."
+        ]
+    ]
 ];
--- assets/locales/fr.php
+++ assets/locales/fr.php
@@ -14,6 +14,25 @@ $lang = [
         "alt-text" => "Deux lettres e balancé l'un sur l'autre"
     ],
     "button" => [
-        "see-more" => "Voir plus d'articles"
+        "see-more" => "Voir plus d'articles",
+        "submit" => "Soumettre"
     ],
+    "admin" => [
+        "h1" => "Insérer un article dans la base de données"
+    ],
+    "form" => [
+        "label" => [
+            "title" => "Titre",
+            "description" => "Description",
+            "image" => "Image",
+            "image-alt" => "Text alternative pour l'image"
+        ],
+        "input" => [
+            "title" => "Un site internet de portfolio personnel",
+            "image-alt" => "Une capture d'écran de mon site montrant la page de connexion"
+        ],
+        "textarea" => [
+            "description" => "J'ai créé un site internet en utilisant PHP."
+        ]
+    ]
 ];
